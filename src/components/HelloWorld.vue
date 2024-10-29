7b397824c86fff50c567f48f185a1b9b3c1cee0c
Jacob Gur <jacob@fareclock.com>
2024-10-29 12:27:46 -0400

    reproduction example of using non-typescript sfc

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+import { markRaw, toRaw } from 'vue'
+
+export default {
+  data() {
+    return {
+      value1: markRaw({}),
+      value2: toRaw({}),
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div>Value 1 = {{ value1 }}</div>
+    <div>Value 2 = {{ value2 }}</div>
+  </div>
+</template>
